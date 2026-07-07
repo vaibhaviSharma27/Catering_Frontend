c1b10c65f649d68fa4201e073b5e6fa7ddd12bab
Vaibhavi <vaibhavisharma.sv2527@gmail.com>
2026-07-07 13:05:50 +0530

    jgctf

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function () {
 
   return (
     <>
-      <main className="lg:pt-[170px] pt-[250px]">
+      <main className="lg:pt-[170px] pt-[300px]">
         <RouterProvider router={routeObj} />
         </main>
     </>
